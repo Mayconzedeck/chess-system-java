6f9ea573ca7be59d32c671c7e40bd38e81282f6a
maycon <maykonlemos2@gmail.com>
2023-08-01 12:28:36 -0300

    First class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,14 @@
 package application;
 
+import boardGame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-
-
-		System.out.println("hello");
+		
+		Position pos = new Position(3, 5);
+		System.out.println(pos);
+		
 	}
 
 }
