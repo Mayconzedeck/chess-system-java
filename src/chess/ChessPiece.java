ac2396b74ef4c50d398b7e938309f3160d7cb02e
maycon <maykonlemos2@gmail.com>
2023-09-13 21:22:43 -0300

    Piece move count

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -7,7 +7,8 @@ import boardGame.Position;
 public abstract class ChessPiece extends Piece {
 
 	private Color color;
-
+	private int moveCount;
+	
 	public ChessPiece(Board board, Color color) {
 		super(board);
 		this.color = color;
@@ -16,7 +17,18 @@ public abstract class ChessPiece extends Piece {
 	public Color getColor() {
 		return color;
 	}
+	public int getMoveCount () {
+		return moveCount;
+	}
 	
+	public void increaseMoveCount() {
+		moveCount++;
+	}
+	
+	public void decreaseMoveCount() {
+		moveCount--;
+	}
+		
 	public ChessPosition getChessPosition() {
 		return ChessPosition.fromPosition(position);
 	}
